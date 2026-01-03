3467a34dd6f28a5a90ea055b490f6484445e0854
Karam2026 <s2510769008@fhooe.at>
2026-01-03 12:42:45 +0100

    Update subtask2.py
    Add code to subtasjk2.py

--- subtask2.py
+++ subtask2.py
@@ -1,1 +1,15 @@
+n = 0
+s = 0
+x = int(input("Enter number: "))
+m = x if x != -1 else -1
+
+while x != -1:
+    s += x
+    n += 1
+    if x < m:
+        m = x
+    x = int(input("Enter number: "))
+
+a = -1 if n == 0 else s / n
+print(n, s, m, a)
 
